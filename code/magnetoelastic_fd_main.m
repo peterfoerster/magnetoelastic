9287184980ab38c8e34ba3c3f5819f8715a56575
peter <peterfrankfoerster@gmail.com>
2020-02-16 13:40:28 +0100

    peter

--- code/magnetoelastic_fd_main.m
+++ code/magnetoelastic_fd_main.m
@@ -1,8 +1,8 @@
 % frequency domain
 pkg load geopdes;
 
-magnetic = 1;
-mechanic = 1;
+magnetic = 0;
+mechanic = 0;
 geometry_file = 'magnetoelastic_v3';
 
 % specify problem and material data
@@ -10,32 +10,32 @@ omega = 2*pi*50;
 [problem_data, method_data] = setup_problem_fd (geometry_file, omega);
 
 tic;
-% [geometry_mec, msh_mec, space_mec, u, msh_mag, space_mag, A] = mp_solve_coupling2d_fd (problem_data, method_data);
-% [geometry_mec, msh_mec, space_mec, msh_mag, space_mag] = mp_rebuild_coupling2d_fd (problem_data, method_data);
+% [geometry_mec, msh_mec, space_mec, u_fd, msh_mag, space_mag, A] = mp_solve_coupling2d_fd (problem_data, method_data);
+[geometry_mec, msh_mec, space_mec, msh_mag, space_mag] = mp_rebuild_coupling2d_fd (problem_data, method_data);
 % [geometry_mec, msh_mag, space_mag, A] = mp_solve_magnetoquasistatics2d (problem_data, method_data);
 % [geometry_mec, msh_mag, space_mag] = mp_rebuild_magnetoquasistatics2d (problem_data, method_data);
 fprintf('\ntime elapsed for solution: %d min\n', toc/60);
-
+return
 % magnetic problem
 if (magnetic)
    save(['A_degree=' num2str(method_data.degree(1)) '_nsub=' num2str(method_data.nsub(1)) '.mat'], 'A');
-   T = 0;
    T = linspace(0, 1/50, 31);
    sp_to_vtk_mp_curl2d_fd (A, space_mag, geometry_mec, method_data.nsub, ['B_degree=' num2str(method_data.degree(1)) '_nsub=' num2str(method_data.nsub(1))], 'B', omega, T);
 end
 
 % mechanic problem
 if (mechanic)
-   save(['u_degree=' num2str(method_data.degree(1)) '_nsub=' num2str(method_data.nsub(1)) '.mat'], 'u');
+   save(['u_fd_degree=' num2str(method_data.degree(1)) '_nsub=' num2str(method_data.nsub(1)) '.mat'], 'u_fd');
    i_cmp = complex(0, 1);
+   u_td = u_fd - u;
    for it=1:length(T)
       % subtract preload first?
-      u_plot = real(u * exp(i_cmp*omega*T(it))) * 1e3;
+      u_plot = (u + real(u_td * exp(i_cmp*omega*T(it)))*1e1) * 1e3;
       figure(it);
       geometry_def = geo_deform_mp (u_plot, space_mec, geometry_mec);
       nrbplot(geometry_def(5).nurbs, method_data.nsub);
       % nrbkntplot(geometry_def(5).nurbs);
-      axis([0 4 2.5 4.5]);
+      axis([0 4.125 2.5 4.5]);
       shading faceted; view(2);
       set(gcf, 'Position', get(0, 'Screensize'));
       drawnow;
